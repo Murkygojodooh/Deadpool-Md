ee9c3eeddbd3e5682e1f1fec20871fa7aa400a3b
Murkygojodooh <murkygojo@gmail.com>
2024-06-03 19:16:19 +0100

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -20,13 +20,13 @@ const chalk = require('chalk')
 
 global.region = 'kenya' // serah
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = '𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = '꧁༒𝑴𝒖𝒓𝒌𝒚 𝒅𝒆𝒔𝒌ꪶ࿋྄ིᤢꫂ𝑮𝒐𝒋𝒐⁖℘༒' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
-global.owner = ['254796283064'] // ubah aja pake nomor lu
+global.owner = ['237698793862'] // ubah aja pake nomor lu
 //==========================zetsuboclient=======================//
 global.botname = '𝗗𝗘𝗔𝗗𝗣𝗢𝗢𝗟 𝗕𝗨𝗚' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.packname = '𝘾𝙤𝙣𝙛𝙧𝙤𝙣𝙩𝙚𝙧' // ubah aja ini nama sticker
-global.author = '𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁' // ubah aja ini nama sticker
+global.packname = '𝓜𝓻. 𝓜𝓓𝓖' // ubah aja ini nama sticker
+global.author = '꧁༒𝑴𝒖𝒓𝒌𝒚 𝒅𝒆𝒔𝒌ꪶ࿋྄ིᤢꫂ𝑮𝒐𝒋𝒐⁖℘༒' // ubah aja ini nama sticker
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sessionName = 'classicsession' //Gausah Juga
 global.sp = '⭔' // Gausah Juga
